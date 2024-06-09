e6bc710a6a588587d00252ed5aeb6cd470fe58c7
chinuiscoder@123 <kirti.hooda2006@gmail.com>
2024-06-09 10:20:14 +0530

    Variables and constant declarations and printing them.

--- Week1/TypeCasting.c
+++ Week1/TypeCasting.c
@@ -1,1 +1,17 @@
-New change in this file
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    // Declaring varoiables
+    int n = 0,i =0;
+    printf("\nElements are\n %d", n);
+
+    // Typecasing it
+
+
+    // input output
+    
+}
