59d8436b27610554eca8a510378c746f25811a42
chinuiscoder@123 <kirti.hooda2006@gmail.com>
2024-06-10 12:09:18 +0530

    putting values inside an array through pointer;;

---
+++ Week2/PuttingValuesInArray.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5];
+    int *ptr = &arr[0];
+    for(int i=0; i<5; i++)
+    {
+        printf("%d index = ",i);
+        scanf("%d",(ptr + i));
+    }
+    for(int i=0; i<5; i++)
+    {
+        printf("%d index = %d\n",i,*(ptr + i));
+    }
+    return 0;
+    }
