8c1b84294ce6973cbae353789f23d9ed387cfdad
chinuiscoder@123 <kirti.hooda2006@gmail.com>
2024-06-17 13:34:29 +0530

    move zero to last

--- Week2/Binary search/MoveZeroesToLast.c
+++ Week2/Binary search/MoveZeroesToLast.c
@@ -1,29 +1,19 @@
 #include <stdio.h>
 
-void movezero(int n, int arr[])
+void moveZeroes(int arr[], int n)
 {
-    int j = -1;
-
+    int count = 0;
     for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == 0)
-        {
-            j = i;
-            break;
-        }
-    }
-
-    if (j == -1)
-        return;
-    for (int i = j + 1; i < n; i++)
     {
         if (arr[i] != 0)
         {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            j++;
+            arr[count++] = arr[i];
         }
+    }
+
+    while (count < n)
+    {
+        arr[count++] = 0;
     }
 }
 
@@ -31,11 +21,14 @@ int main()
 {
     int arr[] = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
-    movezero(n, arr);
+
+    moveZeroes(arr, n);
+
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
     printf("\n");
+
     return 0;
 }
